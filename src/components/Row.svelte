ab651b887f2330be2aedaca33b81f34786484db5
Nathan <natperson@gmail.com>
2022-05-25 05:35:18 -0700

    bounce win animation

--- src/components/Row.svelte
+++ src/components/Row.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { gameState, error } from '../store';
+    import { derived } from 'svelte/store';
+    import { gameState, error, winState } from '../store';
     import { getHintsForGuess } from '../utils';
     export let rowIndex = 0;
     
@@ -7,7 +8,6 @@
     let letters = ['', '', '', '', ''];
     let isCurrentRowActive = true;
     let hasCurrentRowBeenSubmitted = false;
-
 
     gameState.subscribe(newState => {
         const firstUnsubmittedIndex = newState.findIndex(guess => guess.submitted === false);
@@ -29,6 +29,21 @@
         }
         doesCurrentRowHaveError = false;
     });
+
+    let didCurrentRowJustWin = false;
+    derived([gameState, winState], ([$gameState, $winState]) => {
+        if ($winState === true) {
+            const allRowsWithGuesses = $gameState.filter(game => !!game.guess);
+            console.log({ allRowsWithGuesses });
+            return rowIndex === allRowsWithGuesses.length - 1;
+        }
+        return false;
+    }, false).subscribe(val => {
+        if (val) {
+            didCurrentRowJustWin = val
+        }
+    });
+   
     
 </script>
 <div class="game-row">
@@ -38,6 +53,7 @@
                 class="tile {hints[i]}" 
                 class:tbd={!hints[i] && !!letter}
                 class:empty={!hints[i] && !letter}
+                class:win={didCurrentRowJustWin}
             >
                 {letters[i]}
             </div>
@@ -60,7 +76,7 @@
         display: flex;
         align-items: center;
         justify-content: center;
-        font-size: 26px;
+        font-size: 2rem;
         font-weight: 900;
     }
     .empty {
@@ -82,6 +98,76 @@
         100% {
             transform: scale(1);
         }
+    }
 
+
+  .pop {
+    animation-name: PopIn;
+    animation-duration: 100ms;
+  }
+
+  @keyframes PopIn {
+    from {
+      transform: scale(0.8);
+      opacity: 0;
+    }
+
+    40% {
+      transform: scale(1.1);
+      opacity: 1;
+    }
+  }
+
+  .flip-in {
+    animation-name: FlipIn;
+    animation-duration: 250ms;
+    animation-timing-function: ease-in;
+  }
+  @keyframes FlipIn {
+    0% {
+      transform: rotateX(0);
+    }
+    100% {
+      transform: rotateX(-90deg);
+    }
+  }
+  .flip-out {
+    animation-name: FlipOut;
+    animation-duration: 250ms;
+    animation-timing-function: ease-in;
+  }
+  @keyframes FlipOut {
+    0% {
+      transform: rotateX(-90deg);
+    }
+    100% {
+      transform: rotateX(0);
+    }
+  }
+
+  .win {
+      animation-name: Bounce;
+      animation-duration: 1000ms;
+    }
+
+    @keyframes Bounce {
+        0%, 20% {
+        transform: translateY(0);
+        }
+        40% {
+        transform: translateY(-30px);
+        }
+        50% {
+        transform: translateY(5px);
+        }
+        60% {
+        transform: translateY(-15px);
+        }
+        80% {
+        transform: translateY(2px);
+        }
+        100% {
+        transform: translateY(0);
+        }
     }
 </style>
