0207b6ac342b8326e38e8f98c724f2ef43d8a47e
Nathan <natperson@gmail.com>
2022-05-30 16:23:36 -0700

    only show one hint per letter

--- src/utils/getHints.ts
+++ src/utils/getHints.ts
@@ -3,15 +3,26 @@ import { answer } from '../store';
 
 type HINT_TYPE = 'correct' | 'present' | 'absent';
 
+// we only want to show one 'present' hint per number letter occurences
+// remove letter from answer each time we provide a hint to avoid showing
+// multiple 'present' hints when only one letter instance is present
+const omitLetterByIndex = (word: string, index: number) => {
+    return word.slice(0, index) + word.slice(index);
+}
+
 export const getHintsForGuess = (_guess: string) => {
     let guess = _guess.toUpperCase();
     const currentAnswer = get(answer);
+    // use to only show one hint per letter from answer
+    let wordLetters = currentAnswer;
     let hints: HINT_TYPE[] = [];
     for (let i = 0; i < guess.length; i++) {
         if (guess[i] === currentAnswer[i]) {
-            hints = [...hints, 'correct']
+            hints = [...hints, 'correct'];
+            wordLetters = omitLetterByIndex(wordLetters, i)
         } else if (currentAnswer.includes(guess[i])) {
             hints = [...hints, 'present']
+            wordLetters = omitLetterByIndex(wordLetters, i);
         } else {
             hints = [...hints, 'absent']
         }
